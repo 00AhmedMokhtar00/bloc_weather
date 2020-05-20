e9a656a76774ca720fa677838b4ccd1487868eac
Ahmed Mokhtar <ahmedmokhtar7711@gmail.com>
2020-05-20 06:45:51 +0200

    done with the first phase (showing static weather)

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
 
+import 'blocks/blocks.dart';
 import 'blocks/simple_bloc_delegate.dart';
+import 'repositories/repositories.dart';
+import 'widgets/widgets.dart';
 
 void main() {
   BlocSupervisor.delegate = SimpleBlocDelegate();
-  runApp(MyApp());
+
+  final WeatherRepository weatherRepository = WeatherRepository(
+    weatherApiClient: WeatherApiClient(
+      httpClient: http.Client(),
+    ),
+  );
+
+  runApp(App(weatherRepository: weatherRepository));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class App extends StatelessWidget {
+  final WeatherRepository weatherRepository;
+
+  App({Key key, @required this.weatherRepository})
+      : assert(weatherRepository != null),
+        super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bloc Weather',
-
-      home: ,
+      title: 'Flutter Weather',
+      home: BlocProvider(
+        create: (context) =>
+            WeatherBloc(weatherRepository: weatherRepository),
+        child: Weather(),
+      ),
     );
   }
 }
+
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,7 @@
+export 'weather.dart';
+export 'last_updated.dart';
+export 'location.dart';
+export 'combined_weather_temperature.dart';
+export 'temperature.dart';
+export 'city_selection.dart';
+export 'weather_conditions.dart';
